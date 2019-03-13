02bec1e10f4e049e8317f4d4f57de8c60b0be7b7
Josh Ellis <iamjoshellis@gmail.com>
2019-03-13 17:01:09 +1100

    Update demo styles 🎨

--- demo/src/styles.js
+++ demo/src/styles.js
@@ -1,15 +1,20 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
   html,
   body,
   #root {
     margin: 0;
+    padding: 0;
     height: 100%;
     width: 100%;
     overflow-x: hidden;
     overflow-y: auto;
-    box-sizing: border-box;
   }
   #root {
     background: linear-gradient(to bottom right, #4286f4, #373B44);
